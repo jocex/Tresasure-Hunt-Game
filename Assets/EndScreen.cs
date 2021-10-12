eb4241cc746879238a13ec48b49cf2df87963189
jocex <82776763+jocex@users.noreply.github.com>
2021-10-12 15:05:24 -0400

    Final Product
    Finishing touches

---
+++ Assets/EndScreen.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    public void Setup(int score){
+        gameObject.SetActive(true);
+    }
+}
--- Assets/Score.cs
+++ Assets/Score.cs
@@ -7,12 +7,21 @@ public class Score : MonoBehaviour
 {
     public static int score;
     public TMP_Text scoreText;
+    public GameObject EndScreen;
+    public Score ScoreManager;
 
     public static void AddToScore(){
         score+= 1;
     }
 
+    public void GoToEnd(){
+        EndScreen.SetActive(true);
+    }
+
     private void Update(){
         scoreText.text="score: " + score;
+        if (score > 2){
+            ScoreManager.GoToEnd();
+        }
     }
 }
